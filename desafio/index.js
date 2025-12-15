d347840c7e7402bfbd7189b5f8265239f3683e56
nicolescosta <contatonicolecosta@gmail.com>
2025-12-15 07:58:17 -0300

    Desafio Classificador de nível de herói - DIO

--- desafio/index.js
+++ desafio/index.js
@@ -1,2 +1,32 @@
-let nome = "Nicole";
-console.log (nome);
+// Nome do herói
+let nomeHeroi = "Artemis";
+
+// Quantidade de experiência (XP)
+let xpHeroi = 7500;
+
+// Variável que vai guardar o nível
+let nivel = "";
+
+// Estrutura de decisão para definir o nível
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída final
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+
+
